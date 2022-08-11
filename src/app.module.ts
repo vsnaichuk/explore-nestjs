a05b8cc1766d83ebf5f4a8f870638c5247b71e26
Snaichuk Volodymyr <s.volodymyr@apiko.com>
2022-08-11 12:23:17 +0300

    Add packages. Add base resolver and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { LessonModule } from './lesson/lesson.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    LessonModule,
+  ],
   controllers: [],
   providers: [],
 })
